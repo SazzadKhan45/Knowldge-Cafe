7d018e95d59884bab6fb562d7cc1fc2275105917
Sazzadkhan45 <sazzadkhan753@gmail.com>
2025-06-23 10:05:48 +0600

    first commit

---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,10 @@
+
+const Header = () => {
+    return (
+        <div>
+            <h1 className="text-3xl font-bold mt-6 px-5">Knowledge Cafe</h1>
+        </div>
+    );
+};
+
+export default Header;
